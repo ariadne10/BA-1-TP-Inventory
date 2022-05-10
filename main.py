ddec6aa27beef1a06d37b9f3c88bc775f831681f
ariadne10 <66440668+ariadne10@users.noreply.github.com>
2022-05-10 11:15:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ demand = []
 
 input_string = st.text_input()
 
-demand_A585002 = input_string.split()
+demand_A585002 = input_string.split('Enter KG demand for blend A585002 from March 22 - March 23: ')
 
 
 for i in range(len(demand_A585002)):
